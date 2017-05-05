16724d6bf08e7beecf008211d067162f26ccccc5
Alex <alex.mahinko@gmail.com>
2017-05-04 20:30:26 -0400

    Added error messages to /pong requests. Messages proactively deflect blame and panic appropriately behind users back

--- ajax.js
+++ ajax.js
@@ -19,5 +19,17 @@ $(document).ready(function () {
       $('#step3456').append(responseData + " | ");
     })
   })
-
+  $('#button3').click(function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'get',
+      data: {},
+      datatype: 'text'
+    }).done(function(responseData){
+      $('#step3456').append(responseData + " | ");
+    }).fail(function() {
+      console.log("OH GOD WHAT HAVE YOU DONE STEP AWAY FROM THE MACHINE BEFORE YOU KILL US A- oh. Oh, it's a 500 error. It's cool, don't worry, we're fine, someone else's problem.");
+      $('#step3456').append("<p style=\"color:red;\"> Something's wrong. I didn't do it.</p>");
+    })
+  })
 });
